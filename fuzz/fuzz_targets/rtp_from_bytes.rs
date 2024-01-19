2c305f877a44d6067361961a453c7741ebc89740
Matthew Waters <matthew@centricular.com>
2024-01-19 16:45:18 +1100

    packet: support creating a builder from an existing packet

--- fuzz/fuzz_targets/rtp_from_bytes.rs
+++ fuzz/fuzz_targets/rtp_from_bytes.rs
@@ -2,5 +2,14 @@
 use libfuzzer_sys::fuzz_target;
 
 fuzz_target!(|data: &[u8]| {
-    let _packet = rtp_types::RtpPacket::parse(data);
+    if let Ok(packet) = rtp_types::RtpPacket::parse(data) {
+        let mut built = vec![];
+        packet.as_builder().write(&mut built).unwrap();
+        if let Some(padding) = packet.padding() {
+            // ignore any padding as its contents in the original data are undefined
+            assert_eq!(&built[..built.len() - padding as usize], &data[..data.len() - padding as usize]);
+        } else {
+            assert_eq!(&built, &data);
+        }
+    }
 });
